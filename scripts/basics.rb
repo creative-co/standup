c00e81812700ec31cecfa58f7a26aaa3ffb2d840
Ilia Ablamonov <ilia@flamefork.ru>
2010-11-16 20:24:15 +0300

    s3cmd added to basics

--- scripts/basics.rb
+++ scripts/basics.rb
@@ -1,6 +1,11 @@
 Standup.script :node do
   def run
     sudo 'apt-get -qq update'
-    install_packages 'build-essential libreadline5-dev mc'
+    install_packages 'build-essential libreadline5-dev mc s3cmd'
+    
+    with_processed_file script_file('s3cfg') do |file|
+      upload file,
+             :to => "/home/#{scripts.ec2.params.ssh_user}/.s3cfg"
+    end
   end
 end
