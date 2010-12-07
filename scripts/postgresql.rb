dd9c922622dc87e574eef263d9183ab6f199246c
Ilia Ablamonov <ilia@flamefork.ru>
2010-12-07 03:52:02 +0300

    Do not use passwords for postgres. Simplify

--- scripts/postgresql.rb
+++ scripts/postgresql.rb
@@ -2,6 +2,10 @@ Standup.script :node do
   def run
     install_packages 'postgresql-8.4 libpq-dev'
   
+    upload script_file('pg_hba.conf'),
+           :to => '/etc/postgresql/8.4/main/pg_hba.conf',
+           :sudo => true
+  
     upload script_file('postgresql.conf'),
            :to => '/etc/postgresql/8.4/main/postgresql.conf',
            :sudo => true
@@ -11,38 +15,30 @@ Standup.script :node do
     sudo 'service postgresql-8.4 restart'
   end
   
-  def exec_sql sql
-    su_exec 'postgres', "psql -c \"#{sql}\""
-  end
-  
-  def create_user name, password
-    if exec_sql("select * from pg_user where usename = '#{name}'") =~ /1 row/
-      false
+  def exec_sql sql, local = false
+    command = "psql -c \"#{sql}\" -U postgres -w"
+    if local
+      local_exec command
     else
-      exec_sql "create user #{name} with password '#{password}'"
-      true
+      exec command
     end
   end
   
-  def create_database name, owner
-    if exec_sql("select * from pg_database where datname = '#{name}'") =~ /1 row/
+  def create_database name, local = false
+    if exec_sql("select * from pg_database where datname = '#{name}'", local) =~ /1 row/
       false
     else
-      exec_sql "create database #{name} with owner #{owner}"
+      exec_sql "create database #{name}", local
       true
     end
   end
   
   def dump_command database, username = 'postgres', *args
-    "sudo su -c \"pg_dump -c #{database}\" #{username}"
+    "pg_dump -c #{database} -U #{username} -w"
   end
   
   def load_command database, username = 'postgres', *args
-    if username == :local
-      "psql #{database}"
-    else
-      "sudo su -c \"psql #{database}\" #{username}"
-    end
+    "psql #{database} -U #{username} -w"
   end
   
   protected
