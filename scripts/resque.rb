874c86ce67ad8635d89fdb7351fe4e77cdab7bc3
Striker <strikeroff@gmail.com>
2011-03-14 18:31:41 +0300

    - add resque support fix

--- scripts/resque.rb
+++ scripts/resque.rb
@@ -1,6 +1,7 @@
 Standup.script :node do
   def run
     scripts.redis.install_from_resque
+
     path_to_resque_exec = "#{scripts.webapp.app_path}/script/resque"
     upload script_file('resque'),
            :to => path_to_resque_exec,
@@ -8,6 +9,7 @@ Standup.script :node do
     sudo "chown www-data:www-data #{path_to_resque_exec}"
     sudo "chmod +x #{path_to_resque_exec}"
 
+    sudo "mkdir #{scripts.webapp.app_path}/tmp/pids"
     with_processed_file script_file('resque_monit.conf') do |file|
       scripts.monit.add_watch file
     end
