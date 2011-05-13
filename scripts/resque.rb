627766edb606f11e954304be5fe49452606f0d48
sfilatov <filatov.st.work@gmail.com>
2011-05-13 15:39:23 +0400

    fix for HOME environment variable for resque script

--- scripts/resque.rb
+++ scripts/resque.rb
@@ -3,9 +3,12 @@ Standup.script :node do
     scripts.redis.install_from_resque if  sudo('find /etc/init.d/redis-server').match(/No such file or directory/).present?
 
     path_to_resque_exec = "#{scripts.webapp.app_path}/script/resque"
-    upload script_file('resque'),
-           :to => path_to_resque_exec,
-           :sudo => true
+    with_processed_file script_file('resque') do |file|
+      upload file,
+             :to => path_to_resque_exec,
+             :sudo => true
+    end
+
     sudo "chown www-data:www-data #{path_to_resque_exec}"
     sudo "chmod +x #{path_to_resque_exec}"
 
