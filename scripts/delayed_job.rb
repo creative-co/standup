bc70f9c95718e67bf1a59ac4672255e64ff25676
sfilatov <filatov.st.work@gmail.com>
2011-06-08 21:29:20 +0400

    correct delayed job config

--- scripts/delayed_job.rb
+++ scripts/delayed_job.rb
@@ -1,5 +1,12 @@
 Standup.script :node do
   def run
+    path_to_resque_exec = "#{scripts.webapp.app_path}/script/delayed_job"
+    with_processed_file script_file('delayed_job') do |file|
+      upload file,
+             :to => path_to_resque_exec,
+             :sudo => true
+    end
+
     with_processed_file script_file('delayed_job_monit.conf') do |file|
       scripts.monit.add_watch file
     end
