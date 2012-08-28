80a9b738d0536a452f8f16cd5462a932b9f8cd3b
Ilia Ablamonov <ilia@flamefork.ru>
2012-08-28 17:04:12 +0400

    Fix for #17 "standup v0.6.6 crashes on startup"

--- lib/standup/settings.rb
+++ lib/standup/settings.rb
@@ -1,11 +1,13 @@
 if File.exists?('config/standup.yml')
-  class Standup::Settings < Settingslogic
-    source 'config/standup.yml'
-    load!
-
-    aws['account_id'].gsub!(/\D/, '') if aws['account_id']
-    # keypair_file default to ~/.ssh/keypair_name.pem
-    aws['keypair_file'] ||= "#{File.expand_path '~'}/.ssh/#{aws.keypair_name}.pem"
+  module Standup
+    class Settings < Settingslogic
+      source 'config/standup.yml'
+      load!
+  
+      aws['account_id'].gsub!(/\D/, '') if aws['account_id']
+      # keypair_file default to ~/.ssh/keypair_name.pem
+      aws['keypair_file'] ||= "#{File.expand_path '~'}/.ssh/#{aws.keypair_name}.pem"
+    end
   end
 else
   Standup.const_set :Settings, ActiveSupport::HashWithIndifferentAccess.new('nodes' => {})
